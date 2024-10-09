8cc8f9a7d6d7ea4ed6a4e1235027f19a1dfe1d20
A M Rizal <34767112+amrizal94@users.noreply.github.com>
2024-10-09 23:13:37 +0700

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh init pwsh --config 'C:\Users\KK\AppData\Local\Programs\oh-my-posh\themes\kushal.omp.json' | Invoke-Expression
+Import-Module -Name Terminal-Icons
+Import-Module z
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView

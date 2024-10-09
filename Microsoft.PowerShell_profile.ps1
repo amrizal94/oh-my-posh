c739be975586c5d0bcb5f9ec8e5b50b7ac470fb4
A M Rizal <34767112+amrizal94@users.noreply.github.com>
2024-10-09 23:20:19 +0700

    Update Microsoft.PowerShell_profile.ps1
    update:: dinamis variable for modules

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,17 @@
 oh-my-posh init pwsh --config 'C:\Users\KK\AppData\Local\Programs\oh-my-posh\themes\kushal.omp.json' | Invoke-Expression
-Import-Module -Name Terminal-Icons
-Import-Module z
+# Daftar nama modul
+$modules = @('Terminal-Icons', 'z')
+
+# Loop untuk setiap modul
+foreach ($module in $modules) {
+    # Cek apakah modul sudah ada
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        # Jika modul belum ada, install terlebih dahulu
+        Install-Module -Name $module -Force -Scope CurrentUser
+    }
+
+    # Import modul
+    Import-Module $module
+}
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
